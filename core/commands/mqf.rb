1fe8144be29b4a06cd104171ff74de2575886b4f
alexandretk <alexandrekry@gmail.com>
2017-06-16 11:17:48 -0300

    Updating commands - mainly generation

--- core/commands/mqf.rb
+++ core/commands/mqf.rb
@@ -8,12 +8,6 @@
 
 
 
-dirname = File.expand_path(File.dirname(File.dirname(__FILE__)))
-
-# puts dirname
-# ARGV.each do|a|
-#   puts "Argument: #{a}"
-# end
 
 if ((ARGV[0] == "run") and (ARGV[1] =="sender"))
 	puts "Running Sender"
@@ -21,9 +15,8 @@ if ((ARGV[0] == "run") and (ARGV[1] =="sender"))
 	require_relative "../sender/simple_run_sender.rb"
 	srs = SimpleRunSender.new("../../app/")
 	srs.run
-
-
 end
+
 
 if ((ARGV[0] == "run") and (ARGV[1] =="receiver"))
 	puts "Running Receiver"
@@ -31,15 +24,67 @@ if ((ARGV[0] == "run") and (ARGV[1] =="receiver"))
 	require_relative "../receiver/simple_run_receiver.rb"
 	srr = SimpleRunReceiver.new("../../app/")
 	srr.run
-
 end
 
-if ((ARGV[0] == "generate") and (ARGV[1] =="all"))
-	puts "Generating Sender and Receiver"
-	from_path = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}/app_base_files/."
-	to_path = "#{File.expand_path('.')}/app"
-	puts from_path
-	puts to_path
+def createConfigFile(path, framework_generator_path)
+	out_file = File.new(path, "w")
+
+	out_file.puts("module Config")
+	out_file.puts("	@config = {")
+	out_file.puts("		framework_path: '#{framework_generator_path}',")
+	out_file.puts("	}")
+	out_file.puts("	def self.get")
+	out_file.puts("		return @config")
+	out_file.puts("	end")
+	out_file.puts("end")
+
+	# module Config
+	# 	@config = {
+	# 		framework_path: "hello"
+
+	# 	}
+	# 	def self.get
+	# 		return @config
+	# 	end	
+	# end
+
+	out_file.close
+end
+
+if ((ARGV[0] == "new") and (ARGV[1] != nil))
+
+	puts "Generating new project"
+
+	core_path = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}"
+	project_path = "#{File.expand_path('.')}"
+	
+	project_generator_path = core_path+"/generate_files/project/."
+	project_name_path = project_path + "/" + ARGV[1]	
+	project_config_path = project_name_path + "/config/mqf_conf.rb"
+
+	puts project_generator_path
+	puts project_name_path
+	
 	require 'fileutils'
-	FileUtils.cp_r from_path, to_path
+	FileUtils.cp_r project_generator_path, project_name_path
+	createConfigFile(project_config_path, core_path+"/commands/mqf.rb")
+end
+
+
+
+if ((ARGV[0] == "generate") and (ARGV[1] =="all"))
+
+	puts "Generating Sender and Receiver"
+
+	core_path = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}"
+	project_path = "#{File.expand_path('.')}"
+	
+	app_generator_path = core_path + "/generate_files/app/."
+	app_name_path = project_path + "/app"
+
+	puts app_generator_path
+	puts app_name_path
+
+	require 'fileutils'
+	FileUtils.cp_r app_generator_path, app_name_path
 end
